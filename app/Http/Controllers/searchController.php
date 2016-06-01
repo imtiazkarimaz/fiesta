f6fb83f35378a2497d8741437f2c7c1f8d828525
imtiaz karim <1205098.ik@ugrad.cse.buet.ac.bd>
2016-06-01 23:11:36 +0600

    search Final.

--- app/Http/Controllers/searchController.php
+++ app/Http/Controllers/searchController.php
@@ -1,15 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use DB;
-
 use App\Event;
-
 class searchController extends Controller
 {
     //
@@ -18,9 +12,12 @@ class searchController extends Controller
         	//$str_given = array('abcdeggg', 'fghijkggg', 'mnopqrggg', 'stuvwxggg', 'abcujnggg');
        
         	$str_given=DB::table('Event')->pluck('Ename');
-            if (strlen($str)>1) {
+        	
+        	$str = strtolower($str);
+            if (strlen($str)>0) {
     		  for($i=0;$i<count($str_given);$i++)
     		  {
+    		  		$str_given[$i] = strtolower($str_given[$i]);
     		      if(strstr($str_given[$i], $str)){
     		       //	echo '<a href="hi/">'. $str_given[$i]. '</a><br>';
     		       	$event = Event::where('Ename',$str_given[$i])->first();
@@ -30,26 +27,13 @@ class searchController extends Controller
     		  
     		  }
     		}
-    		if (strlen($str)==1) {
-    		 $str = ucfirst($str);  
-    		  for($i=0;$i<count($str_given);$i++)
-    		  {
-    		      if(strstr($str_given[$i], $str)){
-    		       $event = Event::where('Ename',$str_given[$i])->first();
-    		       $id = $event->Event_id;
-    		       echo '<a href="/fiesta/public/req/show/'. $id. '">'. $str_given[$i]. '</a><br>';
-    		      
-    		    }
-    		 }
-            }else {
+           	else {
               //$msg =  "no suggestion";
               //echo $msg;
             }
             
-
         	//return 'got it';
         }
-
         public function showinfo($id){
         	$event = Event::where('Event_id',$id)->first();
         	return view('searched_event', compact('event'));
